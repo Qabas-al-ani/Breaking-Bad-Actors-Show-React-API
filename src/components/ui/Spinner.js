68621b8c0961e2b512710adc7caf62fdfaad9fa0
Qabas al ani <doctorqa@yahoo.com>
2021-10-31 23:22:41 -0400

    added a gif to the spinner

--- src/components/ui/Spinner.js
+++ src/components/ui/Spinner.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import spinner from "../../images/spinner1.gif";
 
 const Spinner = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <img
+      src={spinner}
+      style={{ width: "200px", margin: "auto", display: "block" }}
+      alt="Loading"
+    />
+  );
+};
 
-export default Spinner
+export default Spinner;

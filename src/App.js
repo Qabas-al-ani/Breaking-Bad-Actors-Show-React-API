0a284468f6302cf5f0ec1d2235525e95653f3b87
Qabas al ani <doctorqa@yahoo.com>
2021-10-31 21:45:45 -0400

    added two initial states and useEffect function to the app to get the needed data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Header from "./components/ui/Header";
 
 const App = () => {
-  return <div className="container">hi</div>;
+  const [itemes, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
